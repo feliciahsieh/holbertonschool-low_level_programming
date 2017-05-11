c4f6278b7ef64fa9f650aa13e2059d3ed2fe1f7a
feliciahsieh <felicia@tixwriteoff.com>
2017-05-11 00:34:49 +0000

    added <unistd.h> library for write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//#include <fcntl.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -7,6 +11,6 @@
  */
 int main(void)
 {
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 return (1);
 }
