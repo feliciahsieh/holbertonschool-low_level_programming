52d294b0ac5067a0ea6a7fe11496403bfc7a4fe6
feliciahsieh <felicia@tixwriteoff.com>
2017-05-11 23:07:04 +0000

    first commit

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+			if ((10 * i + j) < 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
