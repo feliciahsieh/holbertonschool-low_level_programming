38afe0e75f5a511ca0f83469fed44a4cab9972b3
feliciahsieh <felicia@tixwriteoff.com>
2017-05-11 21:56:37 +0000

    first commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar('z' - i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
