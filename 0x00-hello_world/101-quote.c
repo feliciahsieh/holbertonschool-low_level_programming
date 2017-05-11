08b5a6171500a98b0aab1752ddba29183a175753
feliciahsieh <felicia@tixwriteoff.com>
2017-05-11 00:56:53 +0000

    more formatting changes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - Entry point
  *
@@ -8,6 +6,9 @@
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-return (1);
+	char myStr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, myStr, sizeof(myStr) - 1);
+
+	return (1);
 }
