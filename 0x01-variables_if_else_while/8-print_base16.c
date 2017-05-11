bfe936fe8602021bb523a8745eecc8970296e68f
feliciahsieh <felicia@tixwriteoff.com>
2017-05-11 22:22:35 +0000

    first commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+	}
+	for (i = 0; i < 6; i++)
+	{
+		putchar('a' + i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
