6d104611a0c5b800298710584ee170bb068abe5c
feliciahsieh <felicia@tixwriteoff.com>
2017-05-12 05:33:54 +0000

    still tired

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,23 +14,30 @@ int main(void)
 	int jtens;
 	int jones;
 
-	for(i=0;i<100;i++)
+	for (i = 0; i < 100; i++)
 	{
 		ones = i % 10;
 		tens = i / 10;
 
-		for(j=i+1;j<100;j++)
+		for (j = i + 1; j < 100; j++)
 		{
-			printf("%d ",i);
-			printf("%d",j);
+			jones = j % 10;
+			jtens = j / 10;
 
-			if(i<98 && j<99)
+			putchar('0' + ones);
+			putchar('0' + tens);
+			putchar(' ');
+
+			putchar('0' + jones);
+			putchar('0' + jtens);
+
+			if (i < 98 && j < 99)
 			{
 				printf(", ");
 			}
 		}
 
-		if((i<=97) && (j<=100))
+		if ((i <= 97) && (j <= 100))
 		{
 			printf(",");
 		}
