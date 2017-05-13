f26245307d269fb0bb11a6eb4a5bf2626fa92a11
feliciahsieh <felicia@tixwriteoff.com>
2017-05-13 23:45:18 +0000

    did I finally get the logic right?

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,39 +7,39 @@
  */
 int main(void)
 {
-	int i;
-	int tens;
-	int ones;
-	int j;
-	int jtens;
-	int jones;
+	int i, tens, ones, j=1, jtens, jones;
 
 	for (i = 0; i < 100; i++)
 	{
 		ones = i % 10;
 		tens = i / 10;
 
+		if (!((i==0) && (j==1)) && (!((i==98) && (j==100))) )
+		{
+			putchar(' ');
+		}
 		for (j = i + 1; j < 100; j++)
 		{
+			if (!((i==0) && (j==1)))
+			{
+				putchar(',');
+				putchar(' ' );
+			}
+
+			if(j==100)
+			{
+				continue;
+			}
 			jones = j % 10;
 			jtens = j / 10;
 
-			putchar('0' + ones);
 			putchar('0' + tens);
+			putchar('0' + ones);
+
 			putchar(' ');
 
-			putchar('0' + jones);
 			putchar('0' + jtens);
-
-			if (i < 98 && j < 99)
-			{
-				printf(", ");
-			}
-		}
-
-		if ((i <= 97) && (j <= 100))
-		{
-			printf(",");
+			putchar('0' + jones);
 		}
 	}
 
