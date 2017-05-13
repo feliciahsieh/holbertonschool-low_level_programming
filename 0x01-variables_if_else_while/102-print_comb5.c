329667a5b8822783c79ac530240abd4a560f77c6
feliciahsieh <felicia@tixwriteoff.com>
2017-05-13 23:58:49 +0000

    more betty prob

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,39 +11,29 @@ int main(void)
 
 	for (i = 0; i < 100; i++)
 	{
-		ones = i % 10;
-		tens = i / 10;
-
-		if (!((i == 0) && (j == 1)) && (!((i == 98) && (j == 100))))
-		{
-			putchar(' ');
-		}
+		ones = i % 10; tens = i / 10;
 		for (j = i + 1; j < 100; j++)
 		{
 			if (!((i == 0) && (j == 1)))
 			{
 				putchar(',');
+			}
+			if (!((i == 0) && (j == 1)) && (!((i == 98) && (j == 100))))
+			{
 				putchar(' ');
 			}
-
 			if (j == 100)
 			{
 				continue;
 			}
-			jones = j % 10;
-			jtens = j / 10;
-
+			jones = j % 10; jtens = j / 10;
 			putchar('0' + tens);
 			putchar('0' + ones);
-
 			putchar(' ');
-
 			putchar('0' + jtens);
 			putchar('0' + jones);
 		}
 	}
-
 	putchar('\n');
-
 	return (0);
 }
