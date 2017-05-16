2f349e774cbc91d239d7d36eeddfd969f923a19f
feliciahsieh <felicia@tixwriteoff.com>
2017-05-16 17:14:44 +0000

    added _putchar statements

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -16,12 +16,15 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		result = 1;
+		_putchar('+');
 	} else if (n < 0)
 	{
 		result = -1;
+		_putchar('-');
 	} else
 	{
 		result = 0;
+		_putchar('0');
 	}
 
 	return (result);
