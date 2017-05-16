fddd013398b19650a624f67e4a2a182cdeb98a4d
feliciahsieh <felicia@tixwriteoff.com>
2017-05-16 16:56:24 +0000

    updated comments

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * print_alphabet_x10 - check the code for Holberton School students.
+ * print_alphabet_x10 - print set of a..z ten times
  *
  * Return: Always 0.
  */
@@ -17,6 +17,4 @@ void print_alphabet_x10(void)
 		}
 		_putchar('\n');
 	}
-
-	return;
 }
