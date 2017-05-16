71b7fe4ca4be27ddec92fefac7d77bf784068fa2
feliciahsieh <felicia@tixwriteoff.com>
2017-05-16 20:06:44 +0000

    first commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * add - add 2 integers
+ *
+ * @a: first integer to add
+ * @b: second integer to add
+ * Return: result of 2 integers
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+}
