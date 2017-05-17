d7578c3c3d1573225ff6793c360d130587551493
feliciahsieh <felicia@tixwriteoff.com>
2017-05-17 18:05:39 +0000

    fixed '0' in > 100 numbers

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,15 +1,12 @@
 #include "holberton.h"
-
 /**
  * print_times_table - prints the n times table, starting with 0
- *
  * @n: type of times table to print
- *
  * Return: nothing
  */
 void print_times_table(int n)
 {
-	int x, y, xHundreds, xTens, xOnes, result;
+	int x, y, xHundreds, xTens, xOnes, res;
 
 	if (!((n > 15) || (n < 0)))
 	{
@@ -17,10 +14,8 @@ void print_times_table(int n)
 		{
 			for (y = 0; y < n + 1; y++)
 			{
-				result = x * y;
-				xHundreds = result / 100;
-				xTens = (result % 100) / 10;
-				xOnes = result % 10;
+				res = x * y; xHundreds = res / 100;
+				xTens = (res % 100) / 10; xOnes = res % 10;
 
 				/* For before the number */
 				if ((xHundreds > 0))
@@ -30,20 +25,15 @@ void print_times_table(int n)
 
 				if ((xTens > 0) && (y != 0))
 					_putchar(xTens + '0');
+				else if ((xTens==0) && (res>100))
+					_putchar('0');
 				else if (y != 0)
 					_putchar(' ');
-
 				_putchar(xOnes + '0'); /* Output Ones digit */
-
 				if (y < 15) /* Output proper separator */
-				{
 					_putchar(',');
-				}
-
 				if ((xOnes < 10) && (y < 15))
-				{
 					_putchar(' ');
-				}
 			}
 			_putchar('\n');
 		}
