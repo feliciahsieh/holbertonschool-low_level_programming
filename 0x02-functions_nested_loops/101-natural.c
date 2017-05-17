7cdd7e2a12ddceeb89a3bd8e7d027313ae76af5d
feliciahsieh <felicia@tixwriteoff.com>
2017-05-17 01:15:24 +0000

    removed unused var

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
-#include "holberton.h"
 
 /**
  * main - computes & prints the sum of all multiples of 3 or 5
  * below 1024 (excluded), followed by a new line.
  *
- * Return: nothing 279483?
+ * Return: nothing 279483? 
  *
  */
 int main(void)
 {
-	int i, j;
+	int i;
 	long int sumfelicia = 0;
 
 	for (i = 1; i < 1024; i++)
