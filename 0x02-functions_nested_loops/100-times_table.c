fc44dc51050ad8725182536868a3dcf27df3f7e6
feliciahsieh <felicia@tixwriteoff.com>
2017-05-17 06:58:18 +0000

    first commit

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include "holberton.h"
+
+/**
+ * times_table - prints the n times table, starting with 0
+ *
+ * Return: nothing
+ */
+void times_table(void)
+{
+	int x;
+	int y;
+	int xTens, xOnes;
+	int result;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			result = x * y;
+			xTens = result / 10;
+			xOnes = result % 10;
+
+			/* For before the number */
+			if ((xTens > 0))
+			{
+				_putchar(xTens + '0');
+			} else if (y != 0)
+			{
+				_putchar(' ');
+			}
+
+
+			/* Output Ones digit */
+			_putchar(xOnes + '0');
+
+			/* Output proper separator */
+			if (y < 9)
+			{
+				_putchar(',');
+			}
+
+			if ((xOnes < 10) && (y < 9))
+			{
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
