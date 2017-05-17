158a278d6bab767f61a18da771e6b419fc30eef1
feliciahsieh <felicia@tixwriteoff.com>
2017-05-17 03:22:07 +0000

    comment fix

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -3,7 +3,7 @@
 /**
  * main - find sum of even fibonacci < 4x10^6
  *
- * Return: zero
+ * Return: 0
  *
  */
 int main(void)
@@ -11,24 +11,15 @@ int main(void)
 	long int fib1 = 1, fib2 = 2, new, sum = 2;
 	int i;
 
-/*	printf("%ld,\n%ld,\n", fib1, fib2); */
 	for (i = 3; i <= 32; i++)
 	{
 		new = fib1 + fib2;
-/*		printf("NEW: %ld", new); */
 
-		if((new%2)==0)
+		if ((new % 2) == 0)
 		{
 			sum += new;
 		}
-/*
-		if (i < 32)
-		{
-			printf(",");
-			printf(" ");
-			printf("%d:\n",i);
-		}
-*/
+
 		fib1 = fib2;
 		fib2 = new;
 	}
