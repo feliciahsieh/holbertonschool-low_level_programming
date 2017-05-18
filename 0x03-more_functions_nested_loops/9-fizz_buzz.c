3269f8b1ded61dcc3696973b3fe20e93f588f347
feliciahsieh <felicia@tixwriteoff.com>
2017-05-18 22:47:46 +0000

    simplified code

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,25 +6,37 @@
  */
 int main(void)
 {
-	int i;
+	int i = 0;
+	int is3 = 0, is5 = 0;
 
-	for (i = 1; i < 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i > 0)
+		if (i > 1)
 			printf(" ");
 
-		if ((i % 3) != 0)
+		if ((i % 3) == 0)
+			is3 = 1;
+		else
+			is3 = 0;
+
+		if ((i % 5) == 0)
+			is5 = 1;
+		else
+			is5 = 0;
+
+		if (is3 == 1)
+			printf("Fizz");
+		if (is5 == 1)
+			printf("Buzz");
+
+		if (!(is3 || is5))
 		{
 			printf("%d", i);
 		}
-		else if ((i % 3) == 0)
-		{
-			printf("Fizz");
-			if ((i % 5) == 0)
-			{
-				printf("Buzz");
-			}
-		}
+
+		/* Reset */
+		is3 = 0;
+		is5 = 0;
 	}
 	printf("\n");
 
