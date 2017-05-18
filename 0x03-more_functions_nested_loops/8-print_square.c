66c2cef9151a999f50a02488e82b435ec9bcadb2
feliciahsieh <felicia@tixwriteoff.com>
2017-05-18 22:22:22 +0000

    first commit

---
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * print_square - prints a square
+ *
+ * Return: None
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (!size)
+	{
+		_putchar('\n');
+	} else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
