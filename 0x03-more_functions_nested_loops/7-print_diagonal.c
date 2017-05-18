917fd893c75a6f6129c20b29526b344b61a89cb1
feliciahsieh <felicia@tixwriteoff.com>
2017-05-18 22:03:11 +0000

    first commit

--- 0x03-more_functions_nested_loops/7-print_diagonal.c
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -8,17 +8,19 @@
  */
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
 	if (n >= 0)
 	{
 		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j < (n - 1); j++)
+			for (j = 0; j < i; j++)
 			{
 				_putchar(' ');
 			}
 			_putchar('\\');
+			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
