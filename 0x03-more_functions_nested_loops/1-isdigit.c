3dbac8def1469bd42278c9864e096cca92cdd50c
feliciahsieh <felicia@tixwriteoff.com>
2017-05-18 21:07:09 +0000

    init var

--- 0x03-more_functions_nested_loops/1-isdigit.c
+++ 0x03-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,14 @@
 #include "holberton.h"
-
 /**
  * _isdigit - checks if value is a digit
+ *
+ * @c: char to check
  *
  * Return: 1 if it's a digit. 0 if not
  */
 int _isdigit(int c)
 {
-	int result;
+	int result=0;
 
 	if ((c >= '0') && (c <= '9'))
 	{
