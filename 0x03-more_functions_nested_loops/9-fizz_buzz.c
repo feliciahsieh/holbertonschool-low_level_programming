632a5db0063a06db64055f018ddd60e21c58e56d
feliciahsieh <felicia@tixwriteoff.com>
2017-05-18 22:34:08 +0000

    first commit

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 100; i++)
+	{
+		if (i > 0)
+			printf(" ");
+
+		if ((i % 3) != 0)
+		{
+			printf("%d", i);
+		}
+		else if ((i % 3) == 0)
+		{
+			printf("Fizz");
+			if ((i % 5) == 0)
+			{
+				printf("Buzz");
+			}
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
