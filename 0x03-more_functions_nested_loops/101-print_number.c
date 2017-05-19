5996e7f4219f5d2f568983cd5fdb8af41d1d0546
feliciahsieh <felicia@tixwriteoff.com>
2017-05-19 04:22:07 +0000

    Revert "compacter"
    This reverts commit 153f3daf4bfe2f14200ce4c9039643b9779061c2.

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -6,15 +6,16 @@
  */
 void print_number(int n)
 {
-	int i, q, n1, myn2, places = 0, value = 1, myDigit, hi10;
+	int i, q, myNum, myn2, places = 0, value = 1, myDigit, highestTen;
 
 	if (n == 0)
 		_putchar('0');
 	else if ((n >= -2147483648) && (n <= 2147483647))
 	{
-		n1 = n, myn2 = n;
+		myNum = n, myn2 = n;
 		if (myn2 < 0)
 			myn2 = -myn2;
+
 		while (myn2 > 0)
 		{
 			myn2 = myn2 / 10;
@@ -22,19 +23,22 @@ void print_number(int n)
 		}
 		if (n < 0)
 		{
-			_putchar('-'), n1 = -n1;
+			_putchar('-');
+			myNum = -myNum;
 		}
 		for (q = 0; q < (places - 1); q++)
 			value = value * 10;
-		hi10 = value;
+		highestTen = value;
 		for (i = 0; i < (places); i++)
 		{
 			if (i < (places - 1))
 			{
-				myDigit = myNum / hi10, _putchar(myDigit + '0');
-				n1 = n1 - (myDigit * hi10), hi10 = hi10 / 10;
+				myDigit = myNum / highestTen;
+				_putchar(myDigit + '0');
+				myNum = myNum - (myDigit * highestTen);
+				highestTen = highestTen / 10;
 			} else
-				_putchar(n1 + '0');
+				_putchar(myNum + '0');
 		}
 	} else
 		_putchar('\n');
