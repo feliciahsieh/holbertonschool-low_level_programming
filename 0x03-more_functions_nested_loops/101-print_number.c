62b5a4e36aa090d42a7f1d07c01015488db9fd19
feliciahsieh <felicia@tixwriteoff.com>
2017-05-19 03:18:14 +0000

    betty

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -1,82 +1,47 @@
 #include "holberton.h"
-int power(int base, int pwr);
 /**
  * print_number - prints an integer
- *
  * @n:  number to print one char at a time
- *
  * Return: none
  */
 void print_number(int n)
 {
-	int i;
-	int isNegative = 0;
-	int myNum;
-	int places = 0;
-	int myDigit;
-	int highestTen;
-
-	/* Range of int -32,768 to 32,767 (5 places) or
-	   -2,147,483,648 to 2,147,483,647 (10 places) */
+	int i, q, myNum, myn2, places = 0, value = 1, myDigit, highestTen;
 
 	if (n != 0)
 	{
-		myNum = n;
-		if (n < 0)
+		myNum = n, myn2 = n;
+		if (myn2 < 0)
+			myn2 = -myn2;
+
+		while (myn2 > 0)
 		{
-			isNegative = 1;
-			myNum = -myNum;
-		}
-		while(myNum>0)
-		{
-			myNum = myNum / 10;
+			myn2 = myn2 / 10;
 			places++;
 		}
 
-		/* print it out */
-		if (isNegative)
-		{
+		if (n < 0)
 			_putchar('-');
-		}
 
 		myNum = n;
 		if (n < 0)
-		{
 			myNum = -myNum;
-		}
 
-		highestTen = power(10, places);
+		for (q = 0; q < (places - 1); q++)
+			value = value * 10;
+
+		highestTen = value;
 		for (i = 0; i < (places); i++)
 		{
-			if (i<(places-1))
+			if (i < (places - 1))
 			{
 				myDigit = myNum / highestTen;
 				_putchar(myDigit + '0');
 				myNum = myNum - (myDigit * highestTen);
 				highestTen = highestTen / 10;
 			} else
-			{
 				_putchar(myNum + '0');
-			}
 		}
 	} else
-	{
 		_putchar('0');
-	}
-}
-
-int power(int base, int powr)
-{
-	int i;
-	int value = 1;
-
-	/* Higher than 10 causes problem */
-	if(powr < 10)
-	{
-		for (i = 0; i < (powr-1); i++)
-		{
-			value = value * base;
-		}
-		return (value);
-	}
 }
