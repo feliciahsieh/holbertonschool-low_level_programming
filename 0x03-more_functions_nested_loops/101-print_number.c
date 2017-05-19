153f3daf4bfe2f14200ce4c9039643b9779061c2
feliciahsieh <felicia@tixwriteoff.com>
2017-05-19 03:28:10 +0000

    compacter

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -6,14 +6,13 @@
  */
 void print_number(int n)
 {
-	int i, q, myNum, myn2, places = 0, value = 1, myDigit, highestTen;
+	int i, q, n1, myn2, places = 0, value = 1, myDigit, hi10;
 
 	if (n != 0)
 	{
-		myNum = n, myn2 = n;
+		n1 = n, myn2 = n;
 		if (myn2 < 0)
 			myn2 = -myn2;
-
 		while (myn2 > 0)
 		{
 			myn2 = myn2 / 10;
@@ -21,22 +20,19 @@ void print_number(int n)
 		}
 		if (n < 0)
 		{
-			_putchar('-');
-			myNum = -myNum;
+			_putchar('-'), n1 = -n1;
 		}
 		for (q = 0; q < (places - 1); q++)
 			value = value * 10;
-		highestTen = value;
+		hi10 = value;
 		for (i = 0; i < (places); i++)
 		{
 			if (i < (places - 1))
 			{
-				myDigit = myNum / highestTen;
-				_putchar(myDigit + '0');
-				myNum = myNum - (myDigit * highestTen);
-				highestTen = highestTen / 10;
+				myDigit = myNum / hi10, _putchar(myDigit + '0');
+				n1 = n1 - (myDigit * hi10), hi10 = hi10 / 10;
 			} else
-				_putchar(myNum + '0');
+				_putchar(n1 + '0');
 		}
 	} else
 		_putchar('0');
