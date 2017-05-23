5762e585a4c81bd4dfdea07408d57299fa96f2e9
feliciahsieh <felicia@tixwriteoff.com>
2017-05-23 16:50:46 +0000

    clean up

--- 0x04-pointers_arrays_strings/5-rev_string.c
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
  * rev_string - print and reverse string
  * @s: string to print and reverse
@@ -9,18 +8,14 @@ void rev_string(char *s)
 {
 	int length = 0, i = 0;
 
-	while (s[i++] != '\0')
+	while (s[i++])
 		length++;
-	printf("l:%d  i:%d\n", length, i);
 
 	for (i = 0; i < length / 2; i++)
 	{
 		s[length] = s[i];
 		s[i] = s[length - i - 1];
 		s[length - i - 1] = s[length];
-		printf("swapped i:%d with %d",i,length-i-1);
-		printf("i:%d  %s\n",i,s);
 	}
-	printf("Final:%s\n",s);
 	s[length] = '\0';
 }
