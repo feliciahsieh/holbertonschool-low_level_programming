33a7993240ddcf463cfd5a9dad118697a661a712
feliciahsieh <felicia@tixwriteoff.com>
2017-05-23 02:18:58 +0000

    first commit

---
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * _strcpy - copies src string to dest string
+ * @dest: destination string
+ * @src: source string
+ * Return: none
+ */
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+
+	return (dest);
+}
