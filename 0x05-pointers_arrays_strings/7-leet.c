d9394ce54baeda9132f1096f2b37821a3690097c
feliciahsieh <felicia@tixwriteoff.com>
2017-05-25 21:35:19 +0000

    first commit

---
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * leet - encode string into 1337
+ * @s: string to encode
+ * Return: text
+ */
+char *leet(char *s)
+{
+	int i = 0;
+
+	for (i = 0; s[i]; i++)
+	{
+		if ((s[i] == 'a') || (s[i] == 'A'))
+			s[i] = '4';
+		else if ((s[i] == 'e') || (s[i] == 'E'))
+			s[i] = '3';
+		else if ((s[i] == 'o') || (s[i] == 'O'))
+			s[i] = '0';
+		else if ((s[i] == 't') || (s[i] == 'T'))
+			s[i] = '7';
+		else if ((s[i] == 'l') || (s[i] == 'L'))
+			s[i] = '1';
+	}
+
+	return (s);
+}
