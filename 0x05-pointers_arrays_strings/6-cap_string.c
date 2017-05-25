d747ab510f458218f00f1dc2aca90ce4f2199b4c
feliciahsieh <felicia@tixwriteoff.com>
2017-05-25 18:12:52 +0000

    first commit

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * cap_string - capitalize all words of a string
+ * @a: string to camel case
+ * Return: string with camel case
+ */
+char *cap_string(char *a)
+{
+	int i;
+
+	if ((a[0] >= 'a') && (a[0] <= 'z'))
+		a[0] += 32;
+	for (i = 1; a[i]; i++)
+	{
+		switch (a[i - 1])
+		{
+		case ' ':
+		case '\n':
+		case '\t':
+		case ',':
+		case ';':
+		case '.':
+		case '!':
+		case '?':
+		case '"':
+		case '(':
+		case ')':
+		case '{':
+		case '}':
+			if ((a[i] >= 'a') && (a[i] <= 'z'))
+				a[i] -= 32;
+			break;
+		default:
+			break;
+		}
+	}
+
+	return (a);
+}
