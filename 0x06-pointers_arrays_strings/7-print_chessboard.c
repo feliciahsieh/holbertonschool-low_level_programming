73f51e62386c0c90f4f23ea7fd2a161640a3b2d6
feliciahsieh <felicia@tixwriteoff.com>
2017-05-30 23:35:11 +0000

    no change

--- 0x06-pointers_arrays_strings/7-print_chessboard.c
+++ 0x06-pointers_arrays_strings/7-print_chessboard.c
@@ -2,14 +2,13 @@
 /**
  * print_chessboard - prints a chessboard
  * @a: chessboard array
- * Return: none
  */
 void print_chessboard(char (*a)[8])
 {
-        int i, j;
+	int i, j;
 
-        for (i = 0; i < 8; i++)
-                for (j = 0; j < 8; j++)
-                        _putchar(a[i][j]);
-	_putchar('\n');
+	for (i = 0; i < 8; i++)
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
 }
