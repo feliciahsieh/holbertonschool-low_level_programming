df5552b57bf553eacf5aa5ba53f0f4183cf144a3
feliciahsieh <felicia@tixwriteoff.com>
2017-05-30 17:39:10 +0000

    first commit

--- 0x06-pointers_arrays_strings/0-memset.c
+++ 0x06-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,19 @@
 #include "holberton.h"
 /**
  * _memset - fills memory with a constant byte
- * @s:
- * @b:
- * @n:
+ * @s: string to modify
+ * @b: character to fill string
+ * @n: number of "b" characters to fill string
  * Return: Always 0.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
 	return (s);
 }
