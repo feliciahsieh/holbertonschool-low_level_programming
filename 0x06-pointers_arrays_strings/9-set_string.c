08dd903da4218f258495ef65cb1b1a36b2553566
feliciahsieh <felicia@tixwriteoff.com>
2017-05-31 01:31:40 +0000

    first commit

---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * set_string - sets value of a pointer to a char
+ * @s: double pointer
+ * @to: pointer to char
+ * Return: none
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
