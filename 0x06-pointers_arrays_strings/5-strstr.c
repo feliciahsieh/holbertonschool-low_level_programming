b6ac52e53a30734e27d35a5330b5ed7fb75db90f
feliciahsieh <felicia@tixwriteoff.com>
2017-05-31 03:02:33 +0000

    betty

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 #include <stddef.h>
-#include <stdio.h>
+/*#include <stdio.h>*/
 /**
  * char *_strstr - locate a substring
  * @haystack: string to search
@@ -15,24 +15,23 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (j = 0; needle[j] && !found; j++)
 		{
-/*			printf("i:%d  haystack[i]:%c  j:%d  needle[j]:%c\n",
-i,haystack[i],j,needle[j]); */
+/* printf("i:%d hays[i]:%c  j:%d  nee[j]:%c\n",i,haystack[i],j,needle[j]); */
 			if (haystack[i] == needle[j])
 			{
-				/*printf("MATCHED 1 CHAR\n");*/
+/*printf("MATCHED 1 CHAR\n");*/
 				found = 0;
 				for (k = 0; needle[k]; k++)
 				{
 					if (haystack[i + k] == needle[j + k])
 					{
 						found++;
-						/*printf("LOOKING GOOD:found:%d\n",found);*/
+/*printf("LOOKING GOOD:found:%d\n",found);*/
 					}
 
 				}
 				if (found == k)
 				{
-                                        /*printf("SUCCESS\n");*/
+/*printf("SUCCESS\n");*/
 					found = 15;
 					goto outerloop;
 				} else
