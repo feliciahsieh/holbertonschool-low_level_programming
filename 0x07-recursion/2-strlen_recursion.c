36646ba9d6eed2b7dbebdd73a44cb56ef431f5ed
feliciahsieh <felicia@tixwriteoff.com>
2017-06-01 18:15:21 +0000

    first commit

---
+++ 0x07-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to find length
+ * Return: int length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (s[0] == '\0')
+		return (0);
+	return (_strlen_recursion(s + 1) + 1);
+}
