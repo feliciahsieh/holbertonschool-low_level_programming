b85cc109f14534216ba686a18bcdd7efc392e3d6
feliciahsieh <felicia@tixwriteoff.com>
2017-06-01 18:00:49 +0000

    betty

--- 0x07-recursion/0-puts_recursion.c
+++ 0x07-recursion/0-puts_recursion.c
@@ -1,25 +1,19 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
  * _puts_recursion - prints a string, followed by a new line.
  * @s: string to print
  * Return: none
  */
-void printC(char c)
-{
-	_putchar(c);
-}
-
 void _puts_recursion(char *s)
 {
 	if (s[0] == '\0')
 	{
-		printC('\n');
+		_putchar('\n');
 		return;
 	}
 	else
 	{
-		printC(s[0]);
+		_putchar(s[0]);
 		_puts_recursion(s + 1);
 	}
 }
