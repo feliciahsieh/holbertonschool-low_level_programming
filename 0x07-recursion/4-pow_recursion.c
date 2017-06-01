26e7725c1165dfb50ba5ebfc5df8bf160a905b49
feliciahsieh <felicia@tixwriteoff.com>
2017-06-01 18:32:01 +0000

    first commit

---
+++ 0x07-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _pow_recursion - calculate x^y
+ * @x: base of power function
+ * @y: exponent of pwer function
+ * Return: result of x^y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	if (x == 1)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
+}
