880a4773c3157cbc1788ff547fd66e3b76afe506
feliciahsieh <felicia@tixwriteoff.com>
2017-06-02 23:25:19 +0000

    first commit

---
+++ 0x07-recursion/100-wildcmp.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * wildcmp - compares 2 strings to see if it matches a wildcard
+ * @s1: test string
+ * @s2: wildcard match rules
+ * Return: 1 if matches wildcard or 0 or not
+ */
+int wildcmp(char *s1, char *s2)
+{
+}
