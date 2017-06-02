03d7b0921aa95b008d6e47bcafcae3302a5dcd7e
feliciahsieh <felicia@tixwriteoff.com>
2017-06-02 03:39:07 +0000

    added multiple functions

---
+++ 0x07-recursion/6-is_prime_number.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+/**
+ * calcPrimeNumber - calc prime number using recursion
+ * @n: number to check if it is prime
+ * @div: divisor to check
+ * Return: 1 if Prime; 0 if not
+ */
+int calcPrimeNumber(int n, int div)
+{
+	if (n == div)
+		return (1);
+
+	/*printf("n:%d div:%d  nMODdiv:%d\n",n, div, n%div);*/
+	if ((n % div) == 0)
+	{
+		return (0);
+	}
+	else
+		return (calcPrimeNumber(n, div + 1));
+
+	return (-50); /* error */
+}
+
+/**
+ * is_prime_number - find if n is a prime number
+ * @n: number to check
+ * Return: 1 if n is Prime; 0 if not
+ */
+int is_prime_number(int n)
+{
+	int result = -50;
+
+	if (n <= 0)         /* error */
+		return (0);
+	else if (n == 1)    /* error */
+		return (0);
+	else
+		return (calcPrimeNumber(n, 2));
+
+	return (result);
+}
