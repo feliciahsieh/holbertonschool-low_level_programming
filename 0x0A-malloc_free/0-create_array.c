931850a457985198c6b795d78bef5ef37520b799
feliciahsieh <felicia@tixwriteoff.com>
2017-06-06 21:10:12 +0000

    check for size > 0

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -10,14 +10,16 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *ptr;
+	char *ptr = NULL;
 	unsigned int i;
 
-	ptr = (char *)malloc(size * sizeof(char));
-	if (ptr == NULL)
-		return (NULL);
-	for (i = 0; i < size; i++)
-		ptr[i] = c;
-
+	if (size > 0)
+	{
+		ptr = (char *)malloc(size * sizeof(char));
+		if (ptr == NULL)
+			return (NULL);
+		for (i = 0; i < size; i++)
+			ptr[i] = c;
+	}
 	return (ptr);
 }
