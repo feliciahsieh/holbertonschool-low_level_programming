def7ddaec790ea2f35eb657e79e6c28306635b15
feliciahsieh <felicia@tixwriteoff.com>
2017-06-07 01:40:14 +0000

    added argv[0] and increased malloc size by 1

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -15,15 +15,17 @@ char *argstostr(int ac, char **av)
 	if ((ac == 0) || (av == NULL))
 		return (NULL);
 
-	for (i = 1; i < ac; i++)
+	for (i = 0; i < ac; i++)
 		for (j = 0; av[i][j]; j++)
+		{
 			n++;
+		}
 
-	s = (char *)malloc((n + ac) * sizeof(char));
+	s = (char *)malloc((n + ac + 1) * sizeof(char));
 	if (s == NULL)
 		return (NULL);
 
-	for (i = 1; i < ac; i++)
+	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j]; j++)
 			s[k++] = av[i][j];
