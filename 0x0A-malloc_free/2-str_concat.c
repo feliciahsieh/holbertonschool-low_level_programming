4f507fedf10c76a5cdb9c1116b05fbe686717ff5
feliciahsieh <felicia@tixwriteoff.com>
2017-06-07 20:45:07 +0000

    set s1 and s2 to empty string earlier

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -8,11 +8,13 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *ptr = "";
+	char *ptr = NULL;
 	int i, j, k = 0;
 
-	if ((s1 == NULL) && (s2 == NULL))
-		return (ptr);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	for (i = 0; s1[i]; i++)
 		;
