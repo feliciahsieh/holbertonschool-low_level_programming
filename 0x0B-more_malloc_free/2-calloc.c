e32f5322d043e6ea138c15d6e3ca1e95fd758585
feliciahsieh <felicia@tixwriteoff.com>
2017-06-08 21:51:32 +0000

    removed sizeof

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -17,11 +17,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	}
 
-	ptr = malloc(nmemb * sizeof(size));
+	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; i < size * nmemb; i++)
+	for (i = 0; i < nmemb; i++)
 		ptr[i] = 0;
 
 	return ((void *)ptr);
