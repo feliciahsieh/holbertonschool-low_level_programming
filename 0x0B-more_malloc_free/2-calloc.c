c1b73f15e3bc729dfd30c29b96180056eb219548
feliciahsieh <felicia@tixwriteoff.com>
2017-06-08 21:17:03 +0000

    first commit

---
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * _calloc - allocate memory for an array
+ * @nmemb: number of elements of each size bytes
+ * @size: size of array
+ * Return: return pointer to array
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *ptr;
+	unsigned int i;
+
+	if ((nmemb == 0) || (size == 0))
+	{
+		return (NULL);
+	}
+
+	ptr = malloc(nmemb * sizeof(size));
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		ptr[i] = 0;
+
+
+	return (ptr);
+}
