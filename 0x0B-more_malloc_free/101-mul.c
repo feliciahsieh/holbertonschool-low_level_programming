d24cc15e0aa06bb6fc224a7d2080e95b31ff03c6
feliciahsieh <felicia@tixwriteoff.com>
2017-06-09 01:14:49 +0000

    checking each char in numbers and updated error msgs

--- 0x0B-more_malloc_free/101-mul.c
+++ 0x0B-more_malloc_free/101-mul.c
@@ -1,19 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include "holberton.h"
 
 int main(int argc, char *argv[])
 {
-	long long int a, n1, n2;
+	long int a, n1, n2, i;
 
-	argc = argc;
-	printf("%s %s %s\n",argv[0], argv[1], argv[2]);
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	for (i = 0; argv[1][i]; i++)
+		if (!isdigit(argv[1][i]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+
+	for (i = 0; argv[2][i]; i++)
+		if (!isdigit(argv[2][i]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
 
 	n1 = atoi(argv[1]);
 	n2 = atoi(argv[2]);
 	a = n1 * n2;
 
-	printf("%lld\n", a);
+	printf("%ld\n", a);
 
 	return (0);
 }
