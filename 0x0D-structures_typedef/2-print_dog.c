8f42175ee5c6511e638d085fb29e1028cd027b16
feliciahsieh <felicia@tixwriteoff.com>
2017-06-13 03:19:01 +0000

    changed conditions around

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -9,17 +9,18 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		if (d->name != NULL)
-			printf("Name: %s\n", d->name);
-		else
+		if (d->name == NULL)
 			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
 
 		if (d->age)
 			printf("Age: %f\n", d->age);
 
-		if (d->owner != NULL)
-			printf("Owner: %s\n", d->owner);
-		else
+		if (d->owner == NULL)
 			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
+
 	}
 }
