a4bc66759053249f5833b8a98f56a033b1a5a89e
feliciahsieh <felicia@tixwriteoff.com>
2017-06-14 17:31:37 +0000

    first commit

---
+++ 0x0E-function_pointers/2-int_index.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#define NOTFND -1
+
+/**
+ * int_index - search for an integer
+ * @array: array of integers to search for
+ * @size: size of array
+ * @cmp: pointer to the functio to be used to compare values
+ * Return: array index of found integer
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, ndx, result = NOTFND;
+
+	if (size <= 0)
+		result = NOTFND;
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			ndx = cmp(array[i]);
+			if (ndx != 0)
+			{
+				result = i;
+				break;
+			}
+			else
+				result = NOTFND;
+		}
+	}
+	return (result);
+}
