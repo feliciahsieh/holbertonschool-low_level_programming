5bc9e38dec0f4985edfba42d64614b8e12cb7b4f
feliciahsieh <felicia@tixwriteoff.com>
2017-06-14 20:34:53 +0000

    added exit()

--- 0x0E-function_pointers/0-print_name.c
+++ 0x0E-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "function_pointers.h"
 /**
  * print_name - print a name
@@ -9,4 +10,6 @@ void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
 		f(name);
+	else
+		exit(98);
 }
