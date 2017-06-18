d01666457d0d50f628eb930b55c15f4581236736
feliciahsieh <felicia@tixwriteoff.com>
2017-06-18 20:39:46 +0000

    no change

---
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+#define NIL_STR "(nil)"
+/**
+ * print_strings - prints strings from a variadic function
+ * @separator: string that separates values in variadic function (delimiter)
+ * @n: number of arguments in the variadic function
+ * @...: variadic arguments
+ * Return: none
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	char *myArg = NULL;
+
+	if (!separator)
+		return;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (i)
+			printf("%s", separator);
+
+		myArg = va_arg(ap, char *);
+		if (myArg)
+			printf("%s", myArg);
+		else
+			printf(NIL_STR);
+	}
+	printf("\n");
+
+	va_end(ap);
+}
