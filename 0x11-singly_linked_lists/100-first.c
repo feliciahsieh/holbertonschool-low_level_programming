0c0502719adc82c8901f4860830fa2ec31da3dd0
feliciahsieh <felicia@tixwriteoff.com>
2017-06-22 23:57:50 +0000

    updated betty comments and text to print out

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 void begin(void) __attribute__((constructor(101)));
 /**
- * begin - code run before main function
- * Return: Always 0.
+ * begin - print text before main function runs
+ * Return: none
  */
 void begin(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my \back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
