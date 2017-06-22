3e2a966959354eeadcd36361675b83bc6a361a40
feliciahsieh <felicia@tixwriteoff.com>
2017-06-22 16:57:22 +0000

    working version

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -5,8 +5,15 @@
  *
  * Return: Always 0
  */
-int main(void)
+size_t print_list(const list_t *h)
 {
+	unsigned int count = 0;
 
-	return (0);
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
 }
