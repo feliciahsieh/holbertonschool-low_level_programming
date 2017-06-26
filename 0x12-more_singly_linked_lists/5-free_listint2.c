4e1ba7d7c83c3292778dfd4f10cda18c897bfabc
feliciahsieh <felicia@tixwriteoff.com>
2017-06-26 18:13:36 +0000

    first commit

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - free a listint_t list and sets head to NULL
+ * @head: head of the listint_t list
+ * Return: none
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (*head == NULL)
+		return;
+
+	if ((*head)->next == NULL)
+		free(*head);
+
+	while (*head != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+
+	*head = NULL;
+}
