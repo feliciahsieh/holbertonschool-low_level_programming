0293f34b5bc0f71fdf743f2fe4930371bd11e1e7
feliciahsieh <felicia@tixwriteoff.com>
2017-06-27 20:38:12 +0000

    changing if statement

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -32,7 +32,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		i++;
 	}
 
-	if ((temp == NULL) || (temp->next == NULL))
+	if (temp == NULL)
 		return (LISTFAIL);
 
 	/* check if at beginning of list */
