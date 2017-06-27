29f0ceaae09b438e757990141fd4d4e066cbdbb1
feliciahsieh <felicia@tixwriteoff.com>
2017-06-27 07:05:47 +0000

    first commit. Doesn't work

---
+++ 0x12-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * print_listint_safe - prints a listint_t linked list
+ * @head: head of hte linked list
+ * Return: number of nodes in list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t count = 0;
+
+	if (head == NULL)
+	{
+		exit(98);
+	}
+
+	printf("hello\n");
+	while (head != NULL)
+	{
+		printf("in while loop  i:%i\n", count);
+		count++;
+		head = head->next;
+	}
+
+	return (count);
+}
