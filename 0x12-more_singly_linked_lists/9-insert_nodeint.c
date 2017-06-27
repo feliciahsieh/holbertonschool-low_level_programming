d0933dde9a33dfaec5646a1701e6c85d6d4bfde0
feliciahsieh <felicia@tixwriteoff.com>
2017-06-27 22:35:17 +0000

    removed comments

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,10 +26,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->next = NULL;
 	if (idx == 0)
 	{
-/*
-		if (*head != NULL)
-			new->next = (*head)->next;
-*/
 		new->next = *head;
 		*head = new;
 		return (new);
