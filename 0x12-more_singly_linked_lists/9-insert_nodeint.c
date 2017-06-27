8fe0d7a068e0e2a9b8950442cc655a3300675eae
feliciahsieh <felicia@tixwriteoff.com>
2017-06-27 23:22:08 +0000

    removed last change. Added test for temp->next == NULL

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,13 +28,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new;
 		return (new);
 	}
-	while ((temp != NULL) && (i < (idx - 1)))
+	while ((temp != NULL) && (i < idx))
 	{
 		prev = temp;
 		temp = temp->next;
 		i++;
 	}
-	if (temp == NULL)
+	if (temp == NULL) || (temp->next == NULL))
 	{
 		free(new);
 		return (NULL);
