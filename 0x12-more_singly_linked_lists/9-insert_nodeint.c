ae8f27d9847b1e1adfd9dc865aad39a974e798b0
feliciahsieh <felicia@tixwriteoff.com>
2017-06-27 23:23:19 +0000

    fixed typo

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,7 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp = temp->next;
 		i++;
 	}
-	if (temp == NULL) || (temp->next == NULL))
+	if ((temp == NULL) || (temp->next == NULL))
 	{
 		free(new);
 		return (NULL);
