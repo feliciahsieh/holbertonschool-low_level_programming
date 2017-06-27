e63f1b31e68e3445d15d0ab2ffe956bc5cc8e98b
feliciahsieh <felicia@tixwriteoff.com>
2017-06-27 17:50:10 +0000

    removed comments and checking for NULL

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -9,25 +9,15 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
-/*
-	if (*head == NULL)
-		return;
 
-	if ((*head)->next == NULL)
-		free(*head);
-
-	while (*head != NULL)
+	if (head && *head)
 	{
-		temp = (*head)->next;
-		free(*head);
-		*head = temp;
-	}
-*/
-	while (*head != NULL)
-	{
-		temp = (*head)->next;
-		free(*head);
-		*head = temp;
+		while (*head != NULL)
+		{
+			temp = (*head)->next;
+			free(*head);
+			*head = temp;
+		}
 	}
 
 	*head = NULL;
