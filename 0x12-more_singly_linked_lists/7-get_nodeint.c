2303b83f077b70839b4e31a4cec4fd7dc6e7b2f4
feliciahsieh <felicia@tixwriteoff.com>
2017-06-27 17:58:56 +0000

    added condition if head==NULL

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -11,6 +11,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *current = NULL;
 	unsigned int i = 0;
 
+	if (head == NULL)
+		return (NULL);
+
 	while ((i < index) && (head->next != NULL))
 	{
 		if (head->next != NULL)
