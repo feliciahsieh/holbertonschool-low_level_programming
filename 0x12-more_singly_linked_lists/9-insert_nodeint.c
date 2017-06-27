d6aa5606146beb1d8856622201bbf0d35f75299d
feliciahsieh <felicia@tixwriteoff.com>
2017-06-27 18:12:34 +0000

    freed new node if can't be added condition

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -6,13 +6,12 @@
  * @head: head of the linked list
  * @idx: index to insert the new node
  * @n: data in the new node
- *
  * Return: new node or NULL (if not possible)
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new = NULL;
-	listint_t *temp = NULL;
+	listint_t *temp = *head;
 	listint_t *prev = NULL;
 	unsigned int index = 0;
 
@@ -26,17 +25,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->n = n;
 	new->next = NULL;
 
-	temp = *head;
-	while (temp->next != NULL)
+	while (temp->next != NULL && (idx != index))
 	{
-		if (index == idx)
-			break;
 		prev = temp;
 		temp = temp->next;
 		index++;
 	}
 	if (index > idx)
+	{
+		free(new);
 		return (NULL);
+	}
 
 	prev->next = new;
 	new->next = temp;
