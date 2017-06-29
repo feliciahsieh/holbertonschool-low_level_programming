343085273d8ee0cd6d7d476e380e29bd744b789a
feliciahsieh <felicia@tixwriteoff.com>
2017-06-29 04:44:55 +0000

    improved logic. Working version

--- 0x12-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x12-more_singly_linked_lists/101-print_listint_safe.c
@@ -26,44 +26,36 @@ void printRemainingList(const listint_t *t, const listint_t *h)
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *hare, *tortoise;
-	size_t count = 0;
+	int size = 0, j = 0;
+	void *p[200];
 
+	p[0] = NULL;
+	p[0] = p[0];
 	if (head == NULL)
+		return (0);
+	for (size = 0; head != NULL; size++)
 	{
-		printf("-> [%p] %d\n", (void *)head, head->n);
-		exit(ERRORCODE);
+		for (j = 0; (j < size) && (p[j] != NULL); j++)
+		{
+			if (p[j] == head)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				return (size);
+			}
+		}
+		p[size] = (void *)head;
+		printf("[%p] %d\n", (void *)head, head->n);
+
+		head = head->next;
+		size++;
 	}
-	tortoise = head;
-	hare = head;
-	while (1)
-	{
-		printf("[%p] %d\n", (void *)tortoise, tortoise->n);
-		if (hare == NULL)
-		{
-			printRemainingList(tortoise->next, hare);
-			break;
-		}
-		hare = hare->next;
-		if (hare == NULL)
-		{
-			printRemainingList(tortoise->next, hare);
-			break;
-		}
-		hare = hare->next;
-		tortoise = tortoise->next;
-		if (hare == tortoise)
-		{
-			printRemainingList(tortoise, hare);
-			break;
-		}
-		count++;
-	}
+/*
 	if (tortoise == hare)
 	{
 		printRemainingList(hare, tortoise);
 		printf("-> [%p] %d\n", (void *)hare, hare->n);
 		exit(ERRORCODE);
 	}
-	return (count);
+*/
+	return (size);
 }
