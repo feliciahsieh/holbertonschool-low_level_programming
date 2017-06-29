7ebf53013cc9d6c0bc2f9ea8c83bbc3019b3bbcf
feliciahsieh <felicia@tixwriteoff.com>
2017-06-29 05:01:01 +0000

    removed double size variable

--- 0x12-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x12-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
+
 /**
  * print_listint_safe - prints a listint_t linked list and finds circular list
  * using Floyd's algorithm (tortoise and the hare)
@@ -9,16 +10,14 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	int size = 0, j = 0;
+	size_t size = 0, j = 0;
 	void *p[200];
 
-	p[0] = NULL;
-	p[0] = p[0];
 	if (head == NULL)
 		return (0);
 	for (size = 0; head != NULL; size++)
 	{
-		for (j = 0; (j < size) && (p[j] != NULL); j++)
+		for (j = 0; j < size; j++)
 		{
 			if (p[j] == head)
 			{
@@ -26,11 +25,9 @@ size_t print_listint_safe(const listint_t *head)
 				return (size);
 			}
 		}
-		p[size] = (void *)head;
+		p[j] = (void *)head;
 		printf("[%p] %d\n", (void *)head, head->n);
-
 		head = head->next;
-		size++;
 	}
 	return (size);
 }
