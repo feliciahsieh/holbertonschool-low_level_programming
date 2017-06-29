f3affae401415cc147bcc9ba30c12ffcd083b15a
feliciahsieh <felicia@tixwriteoff.com>
2017-06-29 03:24:31 +0000

    added code for finding first circular node

--- 0x12-more_singly_linked_lists/103-find_loop.c
+++ 0x12-more_singly_linked_lists/103-find_loop.c
@@ -3,46 +3,50 @@
 #include "lists.h"
 #define ERRORCODE 98
 /**
- * print_listint_safe - prints a listint_t linked list and finds circular list
+ * find_listint_loop - prints a listint_t linked list and finds circular list
  * using Floyd's algorithm (tortoise and the hare)
  * @head: head of the linked list
- * Return: number of nodes in list
+ * Return: address of beginning of loop or NULL if there's no loop
  */
-
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *hare, *tortoise;
+	const listint_t *hare, *tortoise, *temp;
+	size_t count = 0;
 
 	if (head == NULL)
-		return (NULL);
+	{
+		printf("-> [%p] %d\n", (void *)head, head->n);
+		exit(ERRORCODE);
+	}
 	tortoise = head;
 	hare = head;
+	temp = head;
 	while (1)
 	{
 		printf("[%p] %d\n", (void *)tortoise, tortoise->n);
-		if (hare == NULL)
-		{ /* No loop found */
-			printRemainingList(tortoise, hare);
-			return (NULL);
-		}
+		if (hare == NULL) /* No loop found */
+			break;
 		hare = hare->next;
-		if (hare == NULL)
-		{
-			printRemainingList(tortoise, hare);
-			return (NULL);
-		}
+		if (hare == NULL) /* No loop found */
+			break;
 		hare = hare->next;
 		tortoise = tortoise->next;
-		if (hare == tortoise)
-		{ /* Loop found */
-			printRemainingList(tortoise, hare);
-			return (tortoise);
-		}
+		if (hare == tortoise) /* Loop found */
+			break;
 	}
 	if (tortoise == hare)
-	{
+	{	/* Circular list detected */
+		printf("DETECTED\nt:%p h:%p\n", (void *)tortoise, (void *)hare);
+		tortoise = temp;
+		while ((tortoise != hare) && (count < 9))
+		{
+			printf("t:%p %d h:%p %d\n", (void *)tortoise, tortoise->n, (void *)hare, hare->n);
+			tortoise = tortoise->next;
+			hare = hare->next;
+			count++;
+		}
 		printf("-> [%p] %d\n", (void *)hare, hare->n);
-		return (tortoise);
+		return ((listint_t *)hare);
 	}
 	return (NULL);
 }
