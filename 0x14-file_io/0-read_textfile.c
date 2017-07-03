b489aa2808a0cb60d6135202fccd4433d4389a07
feliciahsieh <felicia@tixwriteoff.com>
2017-07-03 19:47:23 +0000

    first commit

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: filename to read text
+ * @letters: size of buffer to store text
+ * Return: size of characters printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t count = 0;
+	int fd;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc((letters + 1) * sizeof(char));
+
+	fd = open(filename, O_RDONLY, 0400);
+	if (fd == -1)
+	{
+		printf("Failed to create and open the file\n");
+		exit(1);
+	}
+
+	count = read(fd, buffer, letters);
+	buffer[letters] = '\0';
+
+	write(STDOUT_FILENO, buffer, letters + 1);
+
+	close(fd);
+
+	return (count);
+}
