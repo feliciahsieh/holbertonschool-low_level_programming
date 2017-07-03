f413c49c47cb8b17b674fe2e193f1e83c5b09060
feliciahsieh <felicia@tixwriteoff.com>
2017-07-03 21:50:44 +0000

    added O_TRUNC flag to open()

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 		return (FAIL);
 	if (text_content == NULL)
 	{
-		fd = open(filename, O_CREAT | O_RDWR, 0600);
+		fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 		if (fd == -1)
 			return (FAIL);
 		close(fd);
@@ -31,7 +31,8 @@ int create_file(const char *filename, char *text_content)
 		return (FAIL);
 	for (i = 0; text_content[i]; i++)
 		buffer[i] = text_content[i];
-	fd = open(filename, O_CREAT | O_RDWR, 0600);
+	buffer[i] = '\0';
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 	{
 		free(buffer);
