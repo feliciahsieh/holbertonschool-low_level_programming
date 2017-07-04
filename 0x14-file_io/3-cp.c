2b9066a7ee8e1fdbb0c4d579712155ea0410be7c
feliciahsieh <felicia@tixwriteoff.com>
2017-07-04 02:14:58 +0000

    fixed random

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#define ERR97  "Usage: cp file_from file_to"
+#define ERR97  "Usage: cp file_from file_to\n"
 #define ERR98  "Error: Can't read from file "
 #define ERR99  "Error: Can't write to "
 #define ERR100 "Error: Can't close fd "
@@ -15,15 +15,15 @@
  */
 int main(int argc, char **argv)
 {
-	int fd_dest, fd_source;
-	int readCount = 1, writeCount = 0;
+	int fd_dest, fd_source, readCount = NUMBYTES, writeCount = 0;
 	char buffer[NUMBYTES] = { 0 };
 
-	writeCount = writeCount;
 	if (argc != 3)
-		if (write(STDERR_FILENO, ERR97, 28) < 0)
-			exit(97);
-	fd_dest = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
+	{
+		write(STDERR_FILENO, ERR97, 29);
+		exit(97);
+	}
+	fd_dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_dest == -1)
 	{
 		dprintf(STDERR_FILENO, "%s%s\n", ERR99, argv[2]);
@@ -35,7 +35,7 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "%s%s\n", ERR98, argv[1]);
 		exit(98);
 	}
-	while (readCount > 0)
+	while (readCount == NUMBYTES)
 	{
 		readCount = read(fd_source, buffer, NUMBYTES);
 		if (readCount == -1)
