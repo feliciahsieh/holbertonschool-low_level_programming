0ead1d92b106b70e46fb49a12473cec7c1200901
feliciahsieh <felicia@tixwriteoff.com>
2017-08-05 01:31:47 +0000

    unfinished

--- 0x16-doubly_linked_lists/102-result.c
+++ 0x16-doubly_linked_lists/102-result.c
@@ -4,17 +4,28 @@
 int main(void)
 {
 	int product, i, j;
+	int d1, d2, d3, d4, d5;
 	FILE *fp;
 
 	fp = fopen("102-result", "w+");
 
-	for (i = 999; i > 0; i--)
+	for (i = 999; i > 998; i--)
+	{
 		for (j = 999; j > 0; j--)
 		{
 			product = i * j;
 			printf("%i * %i = %i\n", i, j, product);
-		}
 
+			d5 = product / 100000;
+			d4 = product / 10000;
+			d3 = product / 1000;
+			d2 = product / 100;
+			d1 = product % 10;
+
+			printf("%d %d %d %d\n", d4, d3, d2, d1);
+			break;
+		}
+	}
 	/*switch to write()*/
 	fprintf(fp,"%s\n", "Hello");
 
