ad120b59f387b96541763c1f1de0e75172454297
feliciahsieh <felicia@tixwriteoff.com>
2017-08-06 23:22:02 +0000

    using create_dnode() and recoded

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,33 +23,6 @@ dlistint_t *create_dnode(int n, dlistint_t *prev, dlistint_t *next)
 }
 
 /**
- * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
- * @head: head of doubly-linked list
- * @index: index of node to retrieve starting at 0
- * Return: address of node or NULL if error
- */
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
-{
-	unsigned int count = 0;
-	dlistint_t *curr = head;
-
-	if (!head)
-		return (NULL);
-	if (index == 0)
-		return (head);
-
-	while ((curr != NULL) && (count < index))
-	{
-		curr = curr->next;
-		count++;
-	}
-	if ((count < index) || (curr == NULL))
-		return (NULL);
-	else
-		return (curr);
-}
-
-/**
  * insert_dnodeint_at_index - inserts a new node at a given position
  * @h: head of doubly-linked list
  * @idx: index for insertion of new node
@@ -58,40 +31,38 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new = NULL, *curr = *h, *localPrev = NULL;
+	dlistint_t *curr = *h, *localPrev = NULL;
 	unsigned int count = 0;
 
 	if ((!h && (idx != 0)) || !(*h))
 		return (NULL);
-	if (idx == 0)
+	if (idx == 0) /* insert at beginning*/
 	{
 		if (!h)
-		{
-			*h = create_dnode(n, NULL, NULL);
-		}
+			*h = create_dnode(n, NULL, NULL); /*case1*/
 		else
-		{
+		{ /*case2, case4*/
 			(*h)->prev = create_dnode(n, NULL, *h);
 			*h = (*h)->prev;
 		}
 		return (*h);
 	}
 	for (curr = *h; curr && (count < idx); curr = curr->next, count++)
-	{
+	{ /*case3, case5, case6, case7*/
 		localPrev = curr;
 	}
-	if ((count == idx) && (curr == NULL))
-	{
+	if ((count == idx) && (curr == NULL)) /*insert at list end*/
+	{ /*case3, case6*/
 		localPrev->next = create_dnode(n, localPrev, NULL);
-		return (new);
+		return (localPrev->next);
 	}
-	if (curr == NULL)
+	if ((count < idx) && (curr == NULL))/*case7*/
 		return (NULL);
 	if (localPrev != NULL)
-	{
-		curr->next = create_dnode(n, curr, curr->next);
-		curr->next->next->prev = curr->next;
+	{ /*case5*/
+		localPrev->next = create_dnode(n, localPrev, curr);
+		curr->prev = localPrev->next;
+		return (localPrev->next);
 	}
-	return (curr->next);
+	return (NULL); /*should never run*/
 }
-
