d469a6bfc69334df3613994c2ad73d06fac85707
feliciahsieh <felicia@tixwriteoff.com>
2017-08-07 21:07:04 +0000

    use *.c instead of _*.c

--- 0x17-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x17-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-gcc -Wall -fPIC -c _*.c
+gcc -Wall -fPIC -c *.c
 gcc -shared -o liball.so *.o
 ls -la liba*
 nm -D --defined-only liball.so
 gcc -Wall -pedantic -Werror -Wextra -L. 0-main.c -lall -o len
-ldd len
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
-ldd len
-./len
