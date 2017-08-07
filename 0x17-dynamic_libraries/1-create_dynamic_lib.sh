092591f7a804ade15a55aa88d49c54d5f3e827f7
feliciahsieh <felicia@tixwriteoff.com>
2017-08-07 21:14:13 +0000

    removed ls -al

--- 0x17-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x17-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 gcc -Wall -fPIC -c *.c
 gcc -shared -o liball.so *.o
-ls -la liba*
 nm -D --defined-only liball.so
 gcc -Wall -pedantic -Werror -Wextra -L. 0-main.c -lall -o len
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
