b84ee9d62137f45f8e0a79d3dc3ae357b1cb7e1d
feliciahsieh <felicia@tixwriteoff.com>
2017-08-07 01:37:20 +0000

    first commit that works

--- 0x16-doubly_linked_lists/102-result.c
+++ 0x16-doubly_linked_lists/102-result.c
@@ -1,33 +1,47 @@
 #include <stdio.h>
-#include <stdlib.h>
 
+/**
+ * main - find palindrome of largest product of two 3-digit numbers
+ * Return: Always 1
+ */
 int main(void)
 {
-	int product, i, j;
-	int d1, d2, d3, d4, d5;
-	FILE *fp;
+	int product, max = 0, i, j, c = 0;
+	int numDigits = 6;
+	int d[6] = { 0 };
 
-	fp = fopen("102-result", "w+");
-
-	for (i = 999; i > 998; i--)
+	for (i = 999; i > 100; i--)
 	{
-		for (j = 999; j > 0; j--)
+		for (j = 999; j > 100; j--)
 		{
 			product = i * j;
-			printf("%i * %i = %i\n", i, j, product);
-
-			d5 = product / 100000;
-			d4 = product / 10000;
-			d3 = product / 1000;
-			d2 = product / 100;
-			d1 = product % 10;
-
-			printf("%d %d %d %d\n", d4, d3, d2, d1);
-			break;
+			c = 0;
+			d[0] = d[1] = d[2] = d[3] = d[4] = d[5] = 0;
+			while (product)
+			{
+				d[c] = product % 10;
+				product /= 10;
+				c++;
+			}
+			product = i * j;
+			numDigits = c;
+			if (numDigits == 6)
+			{
+				if ((d[0] == d[5]) &&
+				    (d[1] == d[4]) &&
+				    (d[2] == d[3]))
+					if (product > max)
+						max = product;
+			}
+			else if (numDigits == 5)
+			{
+				if ((d[0] == d[4]) &&
+				    (d[1] == d[3]))
+					if (product > max)
+						max = product;
+			}
 		}
 	}
-	/*switch to write()*/
-	fprintf(fp,"%s\n", "Hello");
-
-	fclose(fp);
+	printf("%d\n", max);
+	return (1);
 }
