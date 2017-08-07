8345b2be6b838d1bd2e3642ee7e2c329d93467a0
feliciahsieh <felicia@tixwriteoff.com>
2017-08-07 21:27:00 +0000

    removed extra steps

--- 0x17-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x17-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-gcc -Wall -fPIC -c *.c
+gcc -Wall -Wextra -Werror -pedantic -fPIC -c *.c
 gcc -shared -o liball.so *.o
-nm -D --defined-only liball.so
-gcc -Wall -pedantic -Werror -Wextra -L. 0-main.c -lall -o len
-export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
