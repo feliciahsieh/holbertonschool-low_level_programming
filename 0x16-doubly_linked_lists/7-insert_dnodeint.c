e75d89d537b20605ddd533b2ae15445cdebf681a
feliciahsieh <felicia@tixwriteoff.com>
2017-08-07 00:04:19 +0000

    redid first check

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -34,7 +34,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *curr = *h, *localPrev = NULL;
 	unsigned int count = 0;
 
-	if (!(*h && h))
+        if (!h || (!(*h) && (idx != 0)))
 		return (NULL);
 	if (idx == 0) /* insert at list beginning*/
 	{
