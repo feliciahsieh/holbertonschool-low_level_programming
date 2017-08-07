63240232783b15b0ab512b6d64f1943881656761
feliciahsieh <felicia@tixwriteoff.com>
2017-08-07 21:34:55 +0000

    moving -c to the other side

--- 0x17-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x17-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc-4.8 -Wall -Werror -Wextra -pedantic -fpic -c *.c
+gcc-4.8 -Wall -Werror -Wextra -pedantic -c -fpic *.c
 gcc-4.8 -Wall -Werror -Wextra -pedantic -shared -o liball.so *.o
