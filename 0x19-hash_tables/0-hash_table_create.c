2969b1306a4520e985eafe1b37b027a3b40fa989
feliciahsieh <felicia@tixwriteoff.com>
2017-08-31 22:27:33 +0000

    changed permissions and added code to 0-hash_table_create.c

---
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *h = NULL;
+	hash_node_t *hash = NULL;
+
+	if (size == ULONG_MAX)
+		return (NULL);
+
+	h = malloc(sizeof(hash_table_t));
+	if (h == NULL)
+		return (NULL);
+
+	hash = malloc(size * sizeof(hash_node_t *));
+	if (hash == NULL)
+	{
+		free(h);
+		return (NULL);
+	}
+
+	h->array = &hash;
+	return (h);
+}
--- 0x19-hash_tables/hash_tables.h
+++ 0x19-hash_tables/hash_tables.h
@@ -1,3 +1,6 @@
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -26,3 +29,8 @@ typedef struct hash_table_s
 	unsigned long int size;
 	hash_node_t **array;
 } hash_table_t;
+
+
+hash_table_t *hash_table_create(unsigned long int size);
+
+#endif
