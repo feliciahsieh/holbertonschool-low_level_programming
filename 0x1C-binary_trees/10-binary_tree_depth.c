d905935a5cbb77c29a60ada3b641a116b0c9b21c
feliciahsieh <felicia@tixwriteoff.com>
2017-10-23 22:11:03 +0000

    first commit with code

---
+++ 0x1C-binary_trees/10-binary_tree_depth.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - finds depth of a binary tree
+ * @node: pointer to binary tree
+ * Return: depth of tree or 0 if empty tree
+ */
+size_t binary_tree_depth(const binary_tree_t *node)
+{
+	int depth = -1;
+
+	if (!node)
+		return (0);
+	while (node)
+	{
+		node = node->parent;
+		depth++;
+	}
+	return (depth);
+}
