06fabec9f63bfacb8dd307615cbe999cfed1b14e
feliciahsieh <felicia@tixwriteoff.com>
2017-10-23 22:32:57 +0000

    first commit with code

---
+++ 0x1C-binary_trees/11-binary_tree_size.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - finds size of a binary tree
+ * @tree: pointer to root of the binary tree
+ * Return: size of tree or 0 if empty tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	int size = 0;
+
+	if (!tree)
+		return (0);
+
+	size = 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
+
+	return (size);
+}
