245977dee7d344ead0380f7d092e37d11b761da7
feliciahsieh <felicia@tixwriteoff.com>
2017-10-23 21:10:10 +0000

    first commit with code

---
+++ 0x1C-binary_trees/5-binary_tree_is_root.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - check if a node is the root
+ * @node: binary tree node to check
+ * Return: 1 if TRUE. 0 if FALSE
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (!node)
+		return (0);
+	return ((node->parent) ? 0 : 1);
+}
