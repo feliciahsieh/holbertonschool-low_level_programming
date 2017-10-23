f40279d3b6337b0fe852e848608b2723c43f5e5b
feliciahsieh <felicia@tixwriteoff.com>
2017-10-23 21:01:38 +0000

    first commit with code

---
+++ 0x1C-binary_trees/4-binary_tree_is_leaf.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - check if a node is a leaf
+ * @node: binary tree node to check
+ * Return: 1 if TRUE. 0 if FALSE
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if(node && !(node->left) && !(node->right))
+		return (1);
+	else
+		return (0);
+}
