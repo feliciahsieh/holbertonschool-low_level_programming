80ace21dbcbf9785078f02edbe207619156000b6
feliciahsieh <felicia@tixwriteoff.com>
2017-10-23 22:56:12 +0000

    fixed syntax error and check for NULL

---
+++ 0x1C-binary_trees/6-binary_tree_preorder.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - traverses binary tree using preorder
+ * @tree: pointer to binary tree
+ * @func: function pointer to the printing function
+ * Return: none
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree)
+		return;
+
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
---
+++ 0x1C-binary_trees/8-binary_tree_postorder.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - traverses binary tree using post-order
+ * @tree: pointer to binary tree
+ * @func: function pointer to the printing function
+ * Return: none
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree)
+		return;
+
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
