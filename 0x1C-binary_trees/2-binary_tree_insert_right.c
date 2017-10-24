e84c7a20451054e1164798fb6980912e1cf7163d
feliciahsieh <felicia@tixwriteoff.com>
2017-10-24 20:19:45 +0000

    add check for parent==NULL

--- 0x1C-binary_trees/2-binary_tree_insert_right.c
+++ 0x1C-binary_trees/2-binary_tree_insert_right.c
@@ -12,6 +12,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node, *t;
 
+	if (!parent)
+		return (NULL);
 	node = malloc(sizeof(binary_tree_t));
 	if (node == NULL)
 		return (NULL);
