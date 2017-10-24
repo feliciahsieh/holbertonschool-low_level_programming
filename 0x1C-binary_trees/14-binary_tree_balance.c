fda3ebb16339cc2908835df35c0e9d0b09d2c638
feliciahsieh <felicia@tixwriteoff.com>
2017-10-24 21:45:51 +0000

    first commit with code

--- 0x1C-binary_trees/14-binary_tree_balance.c
+++ 0x1C-binary_trees/14-binary_tree_balance.c
@@ -2,6 +2,26 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - finds height of a binary tree
+ * @tree: pointer to binary tree
+ * Return: height of tree or 0 if empty tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t lHeight = 0, rHeight = 0;
+
+	if (!tree)
+		return (0);
+
+	lHeight = binary_tree_height(tree->left);
+	rHeight = binary_tree_height(tree->right);
+	if (lHeight > rHeight)
+		return (1 + lHeight);
+	else
+		return (1 + rHeight);
+}
+
+/**
  * binary_tree_balance - measures the balance factor of a binary tree
  * Balance Factor = height(left subtree) - height(right subtree)
  * @tree: pointer to root of the binary tree
@@ -9,13 +29,14 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int l_subtree = 0, r_subtree = 0;
+	int lSubtree = 0, rSubtree = 0;
 
 	if (!tree)
 		return (0);
+	if (!tree->left && !tree->right)
+	  return (0);
 
-	while (1)
-	{
-		;
-	}
+	lSubtree = binary_tree_height(tree->left);
+	rSubtree = binary_tree_height(tree->right);
+	return (lSubtree - rSubtree);
 }
