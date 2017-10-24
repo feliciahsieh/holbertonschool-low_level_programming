5d7d8969b6cd0286cb89cd23db0cee1c8f1d79d8
feliciahsieh <felicia@tixwriteoff.com>
2017-10-24 21:26:10 +0000

    add comment

--- 0x1C-binary_trees/100-binary_trees_ancestor.c
+++ 0x1C-binary_trees/100-binary_trees_ancestor.c
@@ -4,6 +4,7 @@
  * binary_trees_ancestor - Find lowest common ancestor of two nodes
  * @first: First node
  * @second: Second node
+ * Return: pointer to lowest common ancestor node
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
