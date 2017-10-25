88c41f4f4ef883d1945507c9e5140dacba770c71
feliciahsieh <felicia@tixwriteoff.com>
2017-10-25 21:36:46 +0000

    redoing logic

--- 0x1C-binary_trees/6-binary_tree_preorder.c
+++ 0x1C-binary_trees/6-binary_tree_preorder.c
@@ -8,9 +8,10 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (!tree || !func)
+	if (!tree)
 		return;
-
+	if (!func)
+		return;
 	func(tree->n);
 	binary_tree_preorder(tree->left, func);
 	binary_tree_preorder(tree->right, func);
