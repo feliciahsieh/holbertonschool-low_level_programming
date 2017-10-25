6a29fab80dfbfc13319155a5bacd636048ca7bb0
feliciahsieh <felicia@tixwriteoff.com>
2017-10-25 17:19:19 +0000

    Adding more checks. Ex. root=NULL

--- 0x1C-binary_trees/2-main.c
+++ 0x1C-binary_trees/2-main.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-	binary_tree_t *root;
+	binary_tree_t *root, *root2 = NULL;
 
 	root = binary_tree_node(NULL, 98);
 	root->left = binary_tree_node(root, 12);
@@ -19,5 +19,14 @@ int main(void)
 	binary_tree_insert_right(root->left, 54);
 	binary_tree_insert_right(root, 128);
 	binary_tree_print(root);
+
+	printf("Test: root==NULL\n");
+	binary_tree_insert_right(root2, 88);
+	binary_tree_print(root2);
+	printf("Test: root==1 node\n");
+	root2 = binary_tree_node(NULL, 8);
+	binary_tree_insert_right(root2, 88);
+	binary_tree_print(root2);
+
 	return (0);
 }
