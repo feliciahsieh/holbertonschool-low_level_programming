445acb9a56739ec2827ae9ade6ba846fa3ae3804
feliciahsieh <felicia@tixwriteoff.com>
2017-10-25 21:42:39 +0000

    change it to the long way

--- 0x1C-binary_trees/8-binary_tree_postorder.c
+++ 0x1C-binary_trees/8-binary_tree_postorder.c
@@ -10,6 +10,8 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree)
 		return;
+	if (!func)
+		return;
 
 	binary_tree_postorder(tree->left, func);
 	binary_tree_postorder(tree->right, func);
