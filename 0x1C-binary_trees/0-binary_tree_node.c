99c9d43a34d6a333c7fc540e81cc5ff7b82e4b32
feliciahsieh <felicia@tixwriteoff.com>
2017-10-25 17:08:52 +0000

    shortened if statement

--- 0x1C-binary_trees/0-binary_tree_node.c
+++ 0x1C-binary_trees/0-binary_tree_node.c
@@ -13,7 +13,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *node;
 
 	node = malloc(sizeof(binary_tree_t));
-	if (node == NULL)
+	if (!node)
 		return (NULL);
 	node->n = value;
 	node->parent = parent;
