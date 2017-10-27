cbb80185a35eaf9568dc59937465616100d44d6e
feliciahsieh <felicia@tixwriteoff.com>
2017-10-27 06:10:48 +0000

    fixed parent links

--- 0x1C-binary_trees/111-bst_insert.c
+++ 0x1C-binary_trees/111-bst_insert.c
@@ -16,17 +16,17 @@ bst_t *bst_insert(bst_t **tree, int value)
 	{
 		new = (bst_t *)binary_tree_node((binary_tree_t *)tree, value);
 		*tree = new;
-		return (*tree);
+		return (new);
 	}
 	if (value < (*tree)->n)
 	{
 		(*tree)->left = bst_insert(&((*tree)->left), value);
-		(*tree)->left->parent = (*tree)->left;
+		(*tree)->left->parent = (*tree);
 	}
 	else if (value > (*tree)->n)
 	{
 		(*tree)->right = bst_insert(&((*tree)->right), value);
-		(*tree)->right->parent = (*tree)->right;
+		(*tree)->right->parent = (*tree);
 	}
 	return (*tree);
 }
