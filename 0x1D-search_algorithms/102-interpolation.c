f73e2a58211d41b1ce178537cab95e78e4e3aad9
feliciahsieh <felicia@tixwriteoff.com>
2017-12-05 20:07:42 +0000

    first commit with code

---
+++ 0x1D-search_algorithms/102-interpolation.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include "search_algos.h"
+
+/**
+ * printArray - prints subarray being evaluated
+ * @array: array being inspected
+ * @i: index of array to print
+ */
+void printArray(int *array, size_t size, size_t i)
+{
+	if (i < size)
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+	else
+		printf("Value checked array[%lu] is out of range\n", i);
+}
+
+/**
+ * interpolation_search - search for value in sorted array of int with
+ * Interpolation Search
+ * @array: pointer to the first element of the array to search
+ * @size: number of elements in array
+ * @value: data to search for
+ * Return: index of found value or -1 if value is not found or empty array
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	int v = value;
+	size_t l = 0, h = size - 1;
+	size_t pos = l + (((double)(h - l) / (array[h] - array[l]))
+			    * (v - array[l]));
+
+	if ((size == 0) || (array == NULL))
+		return (-1);
+
+	while ((array[h] != array[l]) && (v >= array[l]) &&
+	       (v <= array[h]))
+	{
+		pos = l + ((v - array[l]) * (h - l) / (array[h] - array[l]));
+		printArray(array, size, pos);
+
+		if (array[pos] < v)
+			l = pos + 1;
+		else if (v < array[pos])
+			h = pos - 1;
+		else
+			return pos;
+	}
+
+	if (v == array[l])
+		return l;
+	else
+	{
+		printArray(array, size, pos);
+		return -1;
+	}
+}
