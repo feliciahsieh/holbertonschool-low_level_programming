27bae4c515a42b5b3787ebb89119bdf4737893ee
feliciahsieh <felicia@tixwriteoff.com>
2019-05-21 04:28:36 +0000

    rewrote code

--- 0x05-pointers_arrays_strings/102-infinite_add.c
+++ 0x05-pointers_arrays_strings/102-infinite_add.c
@@ -12,50 +12,46 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, carryover=0, digit;
-	int longerN=0;
+	int carryover=0, digit, longerN=0, lenN1 = 0, lenN2 = 0;
+	int i = 0;
 
-/*	printf("n1:%c n2:%c r:%c size:%d\n",*n1,*n2,*r,size_r); */
-
-	size_r = size_r;
-
-	for (i = 0; n1[i] != '\0'; i++)
+	r[0] = '\0';
+	for (lenN1 = 0; n1[lenN1] != '\0'; lenN1++)
 		;
-
-	for (j = 0; n2[j] != '\0'; j++)
+	for (lenN2 = 0; n2[lenN2] != '\0'; lenN2++)
 		;
+	if ((lenN1 >= size_r - 1) || (lenN2 >= size_r - 1))
+		return (0);
 
-	printf("******i:%d   j:%d\n",i,j);
+	if (lenN1 > lenN2)
+		longerN = lenN1;
+	else
+		longerN = lenN2;
 
-	/* calc addition */
-	longerN = (i > j) ? i-1 : j-1;
-	k = longerN+1;
-	printf("K:%d\n",k);
-
-	r[k+1] = '\0';
-	printf("**k:%d   r[k]:%d\n",k,r[k]);
-	while(longerN >= 0)
+	lenN1--;
+	lenN2--;
+	for (i = longerN; i > 0; i--)
 	{
-		digit = (n1[longerN] - '0') + (n2[longerN] - '0') + carryover;
-
-		if (digit>=10)
+		digit = 0;
+		if (lenN1 >= 0)
+			digit = n1[lenN1] - '0';
+		if (lenN2 >= 0)
+			digit = digit + n2[lenN2] - '0';
+		digit = digit + carryover;
+		if (digit >= 10)
 		{
 			digit = digit % 10;
 			carryover = 1;
 		}
 		else
 			carryover = 0;
-
-		r[k] = (digit % 10) + '0';
-		printf("**k:%d  n1:%c  n2:%c  digit:%d  r[k]:%d\n",k,n1[longerN],n2[longerN],digit,r[k]);
-
-		k--;
-		longerN--;
+		r[i] = digit + '0';
+		lenN1--;
+		lenN2--;
 	}
-	r[k] = carryover + '0';
-	printf("**k:%d  n1:%c  n2:%c  digit:%d  r[k]:%d\n",k,n1[longerN],n2[longerN],digit,r[k]);
-
-	printf("string:%s  r[0]:%c r[1]:%c\n",r,r[0], r[1]);
+	if (carryover == 1)
+		r[0] = carryover + '0';
+	r[longerN + 1] = '\0';
 
 	return (r);
 }
